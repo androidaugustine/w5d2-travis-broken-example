0f416e60d5bb90dabfd2774a04fb93c9b09b5fd8
androidaugustine <subatomik@gmail.com>
2016-11-16 16:28:26 -0500

    Another Broken One:wq

--- Test.php
+++ Test.php
@@ -5,6 +5,14 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
+
+  	public function testTwoTimesTwo(){
+  		$this->assertEquals(2*2,5);
+  	}
+
+  	public function testTwoTimesFive(){
+  		$this->assertEquals(2*5,11);
+  	}
 }
 
 ?>
